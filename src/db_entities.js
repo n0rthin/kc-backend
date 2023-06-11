82fdbf17d57313904d9249a7cad2e7b1210595c4
Vlad Severin <vlad.severin@wtt-solutions.com>
2023-06-11 16:32:01 +0300

    set rejectUnauthorized to false

--- src/db_entities.js
+++ src/db_entities.js
@@ -1,6 +1,11 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 class Article extends Model {}
